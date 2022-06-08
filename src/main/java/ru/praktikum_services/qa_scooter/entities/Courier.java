91b7bb1edd9ec214d787b3fee8a83ba7db0fe9da
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-06-09 02:49:17 +0300

    Delete unnecessary fields from Courier POJO

--- src/main/java/ru/praktikum_services/qa_scooter/entities/Courier.java
+++ src/main/java/ru/praktikum_services/qa_scooter/entities/Courier.java
@@ -4,8 +4,6 @@ public class Courier {
     private String login;
     private String password;
     private String firstName;
-    private Integer id;
-    private String ordersCount;
 
     public Courier() {
     }
@@ -14,11 +12,6 @@ public class Courier {
         this.login = login;
         this.password = password;
         this.firstName = firstName;
-    }
-
-    public Courier(String login, String password) {
-        this.login = login;
-        this.password = password;
     }
 
     public String getLogin() {
@@ -43,22 +36,6 @@ public class Courier {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getOrdersCount() {
-        return ordersCount;
-    }
-
-    public void setOrdersCount(String ordersCount) {
-        this.ordersCount = ordersCount;
     }
 
     @Override
