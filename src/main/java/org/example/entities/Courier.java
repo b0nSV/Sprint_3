99eb598a0382deb6190793b2696ab45bcbabe429
Sergei Bondarev <bondarev.wt@yandex.ru>
2022-06-08 03:26:33 +0300

    Add Courier POJO

---
+++ src/main/java/org/example/entities/Courier.java
@@ -0,0 +1,72 @@
+package org.example.entities;
+
+public class Courier {
+    private String login;
+    private String password;
+    private String firstName;
+    private Integer id;
+    private String ordersCount;
+
+    public Courier() {
+    }
+
+    public Courier(String login, String password, String firstName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+    }
+
+    public Courier(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrdersCount() {
+        return ordersCount;
+    }
+
+    public void setOrdersCount(String ordersCount) {
+        this.ordersCount = ordersCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Courier{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
+}
